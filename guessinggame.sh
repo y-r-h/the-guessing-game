cbb4c8c44b51e02a48511a414f48e062b3341e22
y-r-h <yukirea.hamano@gmail.com>
2021-05-02 14:10:43 +0900

    added while loop

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,6 +4,20 @@ echo "Start Guessing Game"
 
 echo "How many files are in the current directory?"
 
-read n
+read guess
+
+while [[ $guess -ne $answer ]]
+do
+	if [[ $guess > $answer ]]
+	then
+		echo "Too high"
+	else
+		echo "Too low"
+	fi
+	echo "Guess again"
+	read guess
+done
+
+echo "Correct! There are $guess files in the current directory."
 
 echo "End Guessing Game"
